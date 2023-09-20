632c92cc6871abac5ec97de01a3ebbe3a9666775
yasupeter <peterleeful@gmail.com>
2023-09-20 16:37:37 +0900

    Wed, 9.20 최종 (Spring)

--- src/main/java/com/example/spring20230920/controller/Controller01.java
+++ src/main/java/com/example/spring20230920/controller/Controller01.java
@@ -3,19 +3,27 @@ package com.example.spring20230920.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class Controller01 {
     @RequestMapping("/")
     public void method1() {
-        System.out.println("Controller01.method01");}
-};
+        System.out.println("Controller01.method1");
+    }
 
-@RequestMapping("/path1")
+    @RequestMapping("/path1")
     public void method2() {
-    System.out.println("Controller01.method2");
+        System.out.println("Controller01.method2");
+    }
+
+    // /path2 로 요청 왔을 때 일하는 메소드 작성
+    @RequestMapping("/path2")
     public void method3() {
         System.out.println("Controller01.method3");
     }
-
-
+    
+    @RequestMapping("/path4")
+    public void method4() {
+        System.out.println("Controller01.method4");
+        System.out.println("네번째 메소드");
+    }
 }
-
