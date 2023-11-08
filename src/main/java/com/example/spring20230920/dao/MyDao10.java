dddb66481ac9ce0a80d88ebc983a376b1d44ca2f
yasupeter <peterleeful@gmail.com>
2023-11-08 17:47:03 +0900

    Wed, 11.8 api/main1/sub4 고객아이디로 고객정보 조회

--- src/main/java/com/example/spring20230920/dao/MyDao10.java
+++ src/main/java/com/example/spring20230920/dao/MyDao10.java
@@ -1,11 +1,8 @@
 package com.example.spring20230920.dao;
 
-import com.example.spring20230920.domain.MyDto18Employee;
 import com.example.spring20230920.domain.MyDto34Customer;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 @Mapper
 public interface MyDao10 {
@@ -29,45 +26,4 @@ public interface MyDao10 {
         WHERE customerId = 50
         """)
     MyDto34Customer getCustomer();
-
-    @Select("""
-        SELECT 
-         customerId id,
-         customerName name,
-         contactName,
-         address,
-         city,
-         postalCode, 
-         country
-         FROM customers
-        WHERE customerId = #{id}
-        """)
-    MyDto34Customer getCustomerById(Integer id);
-
-    @Select("""
-            SELECT 
-                employeeId id,
-                lastName,
-                firstName,
-                birthDate,
-                photo,
-                notes
-            FROM employees
-            WHERE employeeId = #{id}
-            """)
-    MyDto18Employee getEmployeeById(Integer id);
-
-    @Select("""
-            SELECT customerId 
-            FROM customers 
-            ORDER BY customerId
-            """)
-    List<Integer> getCustomerIdList();
-
-    @Select("""
-        SELECT employeeId 
-        FROM employees
-        ORDER BY employeeId
-        """)
-    List<Integer> getEmployeeIdList();
 }
